6a18c3508b5c0ba8d90248b111bb8de62fdee054
D3C0D1N3 <mrozek.yura3@gmail.com>
2021-02-23 12:18:46 +0200

    Configured gradle

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,9 @@
-module T.REX.Main {
+module Ucode_TRex.main {
     exports world.ucode;
+    exports world.ucode.controller;
+    exports world.ucode.model;
+    exports world.ucode.view;
+
     requires javafx.controls;
     requires javafx.graphics;
     requires javafx.media;
